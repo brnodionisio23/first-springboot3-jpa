679725ac13ea9cf663fa4b358b16d207bcc6097b
Breno Dionisio <brnodionisio23@gmail.com>
2023-06-02 13:37:38 -0300

    Exception handling - findById

---
+++ src/main/java/com/meuprojeto/projetosb/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.meuprojeto.projetosb.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found. Id " + id);
+    }
+}
